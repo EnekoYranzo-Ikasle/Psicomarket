6b45c3b2e9918a3b13b1113fc88a0a403d2f084f
Ibai <ibai.lopez.resta@ikasle.egibide.org>
2025-10-13 10:43:12 +0200

    Estructura de Model y Controller creada

---
+++ App/controllers/ComercioController.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/BaseController.php';
+require_once __DIR__ . '/../models/ComercioModel.php';
+
+class XxxxxController extends BaseController {
+
+    public function index() {
+        $this->render('index.view.php');
+    }
+
+    public function show() {
+    }
+
+    public function store() {
+    }
+
+    public function destroy() {
+    }
+
+    public function destroyAll() {
+    }
+}
---
+++ App/controllers/ImagenController.php
@@ -0,0 +1,22 @@
+<?php
+require_once __DIR__ . '/BaseController.php';
+require_once __DIR__ . '/../models/ImagenModel.php';
+
+class ImagenController extends BaseController {
+
+    public function index() {
+        $this->render('index.view.php');
+    }
+
+    public function show() {
+    }
+
+    public function store() {
+    }
+
+    public function destroy() {
+    }
+
+    public function destroyAll() {
+    }
+}
