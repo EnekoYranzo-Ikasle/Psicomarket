ba8b605f38997dba7aa58488c291785fe30d473b
EnekoYranzo-Ikasle <eneko.yranzo.48852@ikasle.egibide.org>
2025-10-27 08:45:22 +0100

    Terminar los errorBox

--- App/assets/scripts/accionesAnuncioProducto.js
+++ App/assets/scripts/accionesAnuncioProducto.js
@@ -1,50 +1,41 @@
 function inicializarAcciones() {
-  let acciones = document.querySelectorAll(".acciones .btn");
+  let acciones = document.querySelectorAll('.acciones .btn');
   acciones.forEach((accion) => {
-
-    accion.addEventListener("click", (e) => {
+    accion.addEventListener('click', (e) => {
       let id = e.currentTarget.classList[2];
       switch (e.currentTarget.classList[1]) {
-        case "editar":
-          const anuncio = e.currentTarget.closest(".anuncio");
-          const overlay = anuncio.querySelector(".overlay");
-          overlay.style.display = "flex";
-          overlay.style.justifyContent = "center";
-          overlay.style.alignItems = "center";
+        case 'editar':
+          const anuncio = e.currentTarget.closest('.anuncio');
+          const overlay = anuncio.querySelector('.overlay');
+          overlay.style.display = 'flex';
+          overlay.style.justifyContent = 'center';
+          overlay.style.alignItems = 'center';
 
-          overlay
-            .querySelector(".confirmarEditarAnuncio")
-            .addEventListener("click", () => {
-              editarAnuncio(id, {
-                nombre: overlay.querySelector("input[name='nombre']").value,
-                descripcion: overlay.querySelector("input[name='descripcion']").value,
-              });
-              overlay.style.display = "none";
+          overlay.querySelector('.confirmarEditarAnuncio').addEventListener('click', () => {
+            editarAnuncio(id, {
+              nombre: overlay.querySelector("input[name='nombre']").value,
+              descripcion: overlay.querySelector("input[name='descripcion']").value,
             });
-          overlay
-            .querySelector(".cancelarEditarAnuncio")
-            .addEventListener("click", () => {
-              overlay.style.display = "none";
-            });
+            overlay.style.display = 'none';
+          });
+          overlay.querySelector('.cancelarEditarAnuncio').addEventListener('click', () => {
+            overlay.style.display = 'none';
+          });
           break;
-        case "eliminar":
-          const anuncio2 = e.currentTarget.closest(".anuncio");
-          const overlay2 = anuncio2.querySelector(".overlay2");
-          overlay2.style.display = "flex";
-          overlay2.style.justifyContent = "center";
-          overlay2.style.alignItems = "center";
+        case 'eliminar':
+          const anuncio2 = e.currentTarget.closest('.anuncio');
+          const overlay2 = anuncio2.querySelector('.overlay2');
+          overlay2.style.display = 'flex';
+          overlay2.style.justifyContent = 'center';
+          overlay2.style.alignItems = 'center';
 
-          overlay2
-            .querySelector(".confirmarEliminarAnuncio")
-            .addEventListener("click", () => {
-              eliminarAnuncio(id);
-              overlay2.style.display = "none";
-            });
-          overlay2
-            .querySelector(".cancelarEliminarAnuncio")
-            .addEventListener("click", () => {
-              overlay2.style.display = "none";
-            });
+          overlay2.querySelector('.confirmarEliminarAnuncio').addEventListener('click', () => {
+            eliminarAnuncio(id);
+            overlay2.style.display = 'none';
+          });
+          overlay2.querySelector('.cancelarEliminarAnuncio').addEventListener('click', () => {
+            overlay2.style.display = 'none';
+          });
           break;
       }
     });
@@ -54,42 +45,40 @@ function inicializarAcciones() {
 async function eliminarAnuncio(idComercio) {
   try {
     const res = await fetch(
-      "index.php?controller=ComercioController&accion=eliminarAnuncio&id=" +
-        idComercio
+      'index.php?controller=ComercioController&accion=eliminarAnuncio&id=' + idComercio
     );
     if (!res.ok) {
-      throw new Error("Error al eliminar el anuncio");
+      throw new Error('Error al eliminar el anuncio');
     }
     const data = await res.json();
     if (data) {
       window.location.reload();
     }
   } catch (error) {
-    console.error("Error al eliminar el anuncio:", error);
+    showError('Error al eliminar el anuncio:', error);
   }
 }
 
 async function editarAnuncio(idComercio, nuevosDatos) {
   try {
     const res = await fetch(
-      "index.php?controller=ComercioController&accion=editarAnuncio&id=" +
-        idComercio,
+      'index.php?controller=ComercioController&accion=editarAnuncio&id=' + idComercio,
       {
-        method: "POST",
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(nuevosDatos),
       }
     );
     if (!res.ok) {
-      throw new Error("Error al editar el anuncio");
+      throw new Error('Error al editar el anuncio');
     }
     const data = await res.json();
     if (data) {
       window.location.reload();
     }
   } catch (error) {
-    console.error("Error al editar el anuncio:", error);
+    showError('Error al editar el anuncio:', error);
   }
 }
